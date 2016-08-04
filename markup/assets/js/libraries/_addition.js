b15420929ae158c8df8848fc067296af2887c024
rooossoneri <andreykompanietc@gmail.com>
2016-08-04 17:28:36 +0300

    information, news, simple-news pages and styles for cabinets

--- markup/assets/js/libraries/_addition.js
+++ markup/assets/js/libraries/_addition.js
@@ -184,3 +184,12 @@ $(".previous").click(function(){
 $(".submit").click(function(){
   return false;
 })
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        this.classList.toggle("active");
+        this.nextElementSibling.classList.toggle("show");
+    }
+}
