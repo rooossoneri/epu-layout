a21f62010ba2b2a728c472b751b58274d54073c1
rooossoneri <andreykompanietc@gmail.com>
2016-08-10 19:31:28 +0300

    fixes to main and cabinet

--- markup/assets/js/libraries/_addition.js
+++ markup/assets/js/libraries/_addition.js
@@ -1,14 +1,9 @@
 /*
  CODE BY ALEX YATSENKO
 */
-
 /* BIND POPUP OPEN FOR CLICK ON #eventClickElementID */
-
-
-function setPopupOpenEvent(popupElementID, eventClickElementID)
-{
-  jQuery('#'+ eventClickElementID).click(function(event)
-  {
+function setPopupOpenEvent(popupElementID, eventClickElementID) {
+  jQuery('#' + eventClickElementID).click(function(event) {
     $('#' + popupElementID).show();
     event.preventDefault();
     return 0;
@@ -16,63 +11,97 @@ function setPopupOpenEvent(popupElementID, eventClickElementID)
   return 1;
 }
 
-function setDropDown(labelTitleElement, variablesListElement)
-{
-  $(labelTitleElement).click(function()
-  {
-    if($(this).parent(). parent(). find(variablesListElement).css('display') == 'none')
+function setDropDown(labelTitleElement, variablesListElement) {
+  $(labelTitleElement).click(function() {
+    if ($(this).parent().parent().find(variablesListElement).css('display') == 'none')
       $(this).parent().parent().find(variablesListElement).show();
     else
       $(this).parent().parent().find(variablesListElement).hide();
-
-
-
-    return ;
+    return;
   });
 }
 
-jQuery(document).ready(function($)
-{ 
+var selector = '.language-switcher a';
 
-  $('.close-popup').click(function()
-  {
+$(selector).on('click', function(){
+    $(selector).removeClass('active');
+    $(this).addClass('active');
+});
+
+var selector1 = '.types a';
+
+$(selector1).on('click', function(){
+    $(selector1).removeClass('active');
+    $(this).addClass('active');
+});
+
+var selector2 = '.sidebar-menu a';
+
+$(selector2).on('click', function(){
+    $(selector2).removeClass('active');
+    $(this).addClass('active');
+});
+
+var selector3 = '.view a';
+
+$(selector3).on('click', function(){
+  $(selector3).removeClass('active');
+  $(this).addClass('active');
+  $('.tender').addClass('tender-line');
+});
+
+var selector4 = '.view a.square-view';
+
+$(selector4).on('click', function(){
+  $('.tender').removeClass  ('tender-line');
+});
+
+
+// $('a.lines-view').on('click', function(){
+//   $('.tender').addClass('.tender-line');
+// });
+
+
+
+jQuery(document).ready(function($) {
+
+  $('.close-popup').click(function() {
     $(this).parent().hide();
     return 0;
   });
-  $('.close-popups').click(function()
-  {
+  $('.close-popups').click(function() {
     $(this).parent().parent().hide();
     return 0;
   });
-  setPopupOpenEvent('productsIDTypePopup', 'CPVPopup');
+  setPopupOpenEvent('CPVPopup', 'productsIDTypePopup');
   setPopupOpenEvent('auth-popup', 'auth-button');
 
   setDropDown('ul.tree__list li.tree__list-item label span.item-text', 'ul.tree__list');
   setDropDown('dropdown-toggle', 'hidden-types');
 });
 
-jQuery(function(){
- jQuery('#date_timepicker_start').datetimepicker({
-  format:'Y/m/d',
-  onShow:function( ct ){
-   this.setOptions({
-    maxDate:jQuery('#date_timepicker_end').val()?jQuery('#date_timepicker_end').val():false
-   })
-  },
-  timepicker:false
- });
- jQuery('#date_timepicker_end').datetimepicker({
-  format:'Y/m/d',
-  onShow:function( ct ){
-   this.setOptions({
-    minDate:jQuery('#date_timepicker_start').val()?jQuery('#date_timepicker_start').val():false
-   })
-  },
-  timepicker:false
- });
+jQuery(function() {
+  jQuery('#date_timepicker_start').datetimepicker({
+    format: 'Y/m/d',
+    onShow: function(ct) {
+      this.setOptions({
+        maxDate: jQuery('#date_timepicker_end').val() ? jQuery('#date_timepicker_end').val() : false
+      })
+    },
+    timepicker: false
+  });
+  jQuery('#date_timepicker_end').datetimepicker({
+    format: 'Y/m/d',
+    onShow: function(ct) {
+      this.setOptions({
+        minDate: jQuery('#date_timepicker_start').val() ? jQuery('#date_timepicker_start').val() : false
+      })
+    },
+    timepicker: false
+  });
 });
 
- $('.registration').on('click', function() {
+$('.registration').on('click', function() {
   $('.first').hide();
   $('.second').show();
 });
@@ -82,8 +111,8 @@ $('.login').on('click', function() {
   $('.second').hide();
 });
 
-$('.reg-dropdown').on('click', function(){
-  if($('.hidden-types').hasClass('active'))
+$('.reg-dropdown').on('click', function() {
+  if ($('.hidden-types').hasClass('active'))
     $('.hidden-types').removeClass('active');
   else
     $('.hidden-types').addClass('active');
@@ -93,35 +122,46 @@ $('.reg-dropdown').on('click', function(){
 //     $(".location-dropdown").slideToggle("fast");
 // })
 
- $(".location-dropdown").click(function(event){
+$(".location-dropdown").click(function(event) {
   event.stopPropagation();
- });
-
-$(".location").on("click", function (event) {
-    event.stopPropagation();
-    // $(".location-dropdown").slideToggle("fast");
-    $(".location-dropdown").slideToggle("fast");
 });
 
-$(document).on("click", function () {
-    $(".location-dropdown").hide();
+$(".location").on("click", function(event) {
+   $(".money-dropdown").hide();
+   $(".calendar-picker").hide();
+  event.stopPropagation();
+  // $(".location-dropdown").slideToggle("fast");
+  $(".location-dropdown").slideToggle("fast");
 });
 
-$(".calendar").click(function(){
-    $(".calendar-picker").toggle();
+
+$(".calendar").click(function(event) {
+   $(".location-dropdown").hide();
+  $(".money-dropdown").hide();
+  event.stopPropagation();
+  $(".calendar-picker").slideToggle("fast");
 })
 
- $(".calendar-picker").click(function(event){
+$(".calendar-picker").click(function(event) {
   event.stopPropagation();
- });
+});
 
- $(".money").click(function(){
-    $(".money-dropdown").toggle();
+$(".money").click(function(event) {
+  event.stopPropagation();
+  $(".money-dropdown").slideToggle("fast");
+   $(".location-dropdown").hide();
+    $(".calendar-picker").hide();
 })
 
- $(".money-dropdown").click(function(event){
+$(".money-dropdown").click(function(event) {
   event.stopPropagation();
- });
+});
+$(document).on("click", function() {
+  $(".location-dropdown").hide();
+  $(".money-dropdown").hide();
+   $(".calendar-picker").hide();
+});
+
 // $(document).mouseup(function (e)
 // {
 //     var container = $(".location-dropdown, .calendar-picker, .money-dropdown");
@@ -135,141 +175,124 @@ $(".calendar").click(function(){
 
 // ACTIVE ON SIDEBAR MENU
 
-$(document).ready(function(){
-  $('.sidebar-menu a').click(function(){
+$(document).ready(function() {
+  $('.sidebar-menu a').click(function() {
     $('.sidebar-menu a').removeClass("active");
     $(this).addClass("active");
-});
+  });
 });
 
 $(document).ready(function() {
-    $('select').material_select();
-  });
+  $('select').material_select();
+});
 
 
 
-//jQuery time
-var current_fs, next_fs, previous_fs; //fieldsets
-var left, opacity, scale; //fieldset properties which we will animate
-var animating; //flag to prevent quick multi-click glitches
+var current_fs, next_fs, previous_fs;
+var left, opacity, scale;
+var animating;
 
-$(".next").click(function(){
-  if(animating) return false;
+$(".next").click(function() {
+  if (animating) return false;
   animating = true;
-  
+
   current_fs = $(this).parent();
   next_fs = $(this).parent().next();
-  
-  //activate next step on progressbar using the index of next_fs
+
+
   $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
-  
-  //show the next fieldset
-  next_fs.show(); 
-  //hide the current fieldset with style
-  current_fs.animate({opacity: 0}, {
+  next_fs.show();
+  current_fs.animate({
+    opacity: 0
+  }, {
     step: function(now, mx) {
-      //as the opacity of current_fs reduces to 0 - stored in "now"
-      //1. scale current_fs down to 80%
       scale = 1 - (1 - now) * 0.2;
-      //2. bring next_fs from the right(50%)
-      left = (now * 50)+"%";
-      //3. increase opacity of next_fs to 1 as it moves in
+      left = (now * 50) + "%";
       opacity = 1 - now;
       current_fs.css({
-        'transform': 'scale('+scale+')'
-        // 'position': 'absolute'
+        'transform': 'scale(' + scale + ')'
       });
-      next_fs.css({'left': left, 'opacity': opacity});
-    }, 
-    duration: 800, 
-    complete: function(){
+      next_fs.css({
+        'left': left,
+        'opacity': opacity,
+      });
+    },
+    duration: 0,
+    complete: function() {
       current_fs.hide();
       animating = false;
-    }, 
-    //this comes from the custom easing plugin
-    easing: 'easeInOutBack'
+    },
+    easing: 'linear'
   });
 });
 
-$(".previous").click(function(){
-  if(animating) return false;
+$(".previous").click(function() {
+  if (animating) return false;
   animating = true;
-  
   current_fs = $(this).parent();
   previous_fs = $(this).parent().prev();
-  
-  //de-activate current step on progressbar
   $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
-  
   //show the previous fieldset
-  previous_fs.show(); 
+  previous_fs.show();
   //hide the current fieldset with style
-  current_fs.animate({opacity: 0}, {
+  current_fs.animate({
+    opacity: 0
+  }, {
     step: function(now, mx) {
-      //as the opacity of current_fs reduces to 0 - stored in "now"
-      //1. scale previous_fs from 80% to 100%
       scale = 0.8 + (1 - now) * 0.2;
-      //2. take current_fs to the right(50%) - from 0%
-      left = ((1-now) * 50)+"%";
-      //3. increase opacity of previous_fs to 1 as it moves in
+      left = ((1 - now) * 50) + "%";
       opacity = 1 - now;
-      current_fs.css({'left': left});
-      previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
-    }, 
-    duration: 800, 
-    complete: function(){
+      current_fs.css({
+        'left': left
+      });
+      previous_fs.css({
+        'transform': 'scale(' + scale + ')',
+        'opacity': opacity,
+      });
+    },
+    duration: 0,
+    complete: function() {
       current_fs.hide();
       animating = false;
-    }, 
-    //this comes from the custom easing plugin
-    easing: 'easeInOutBack'
+    },
+    easing: 'linear'
   });
 });
 
-$('.sidebar-menu nav ul li a').click(function(){
-    $('.sidebar-menu nav ul li a').removeClass("active");
-    $(this).addClass("active");
+$('.sidebar-menu nav ul li a').click(function() {
+  $('.sidebar-menu nav ul li a').removeClass("active");
+  $(this).addClass("active");
 });
 
-$(".submit").click(function(){
+$(".submit").click(function() {
   return false;
 })
 var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-    acc[i].onclick = function(){
-        this.classList.toggle("active");
-        this.nextElementSibling.classList.toggle("show");
-    }
+  acc[i].onclick = function() {
+    this.classList.toggle("active");
+    this.nextElementSibling.classList.toggle("show");
+  }
 }
 
 function openNav() {
-    document.getElementById("mySidenav").style.width = "90%";
-    document.getElementById("main").style.marginLeft = "90%";
+  document.getElementById("mySidenav").classList.add("main-menu");
+  document.getElementById("main").classList.add("main-menu");
 }
 
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
+  document.getElementById("mySidenav").classList.remove("main-menu");
+  document.getElementById("main").classList.remove("main-menu");
 }
 
-// function openSidebar() {
-//     document.getElementById("sidebar").style.width = "270px";
-//     document.getElementById("main").style.marginLeft = "270px";
-// }
-
-// function closeSidebar() {
-//     document.getElementById("sidebar").style.width = "0";
-//     document.getElementById("main").style.marginLeft= "0";
-// }
-
 function openSidebar() {
-    document.getElementById("sidebar").classList.add("side-bar");
-    document.getElementById("main").classList.add("left-bar");
+  document.getElementById("sidebar").classList.add("side-bar");
+  document.getElementById("main").classList.add("left-bar");
 }
 
 function closeSidebar() {
-    document.getElementById("sidebar").classList.remove("side-bar");
-    document.getElementById("main").classList.remove("left-bar");
+  document.getElementById("sidebar").classList.remove("side-bar");
+  document.getElementById("main").classList.remove("left-bar");
 }
